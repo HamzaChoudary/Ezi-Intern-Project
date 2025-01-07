928dce94bced528929538640255080fd0ff4cb2f
Front-End Project <hamzasarwar1971@gmil.com>
2025-01-07 23:07:05 +0500

    Avo-Clone file Updated

--- avo-website/src/Components/Hero.jsx
+++ avo-website/src/Components/Hero.jsx
@@ -139,7 +139,7 @@ const Hero = () => {
   {/* Overlay Text */}
   <div className="absolute w-full h-full flex items-center justify-center text-white px-6">
     <motion.div
-      className="flex flex-col justify-center space-y-4 px-6 md:px-16 lg:pl-56 lg:pr-48"
+      className="flex flex-col justify-center space-y-4 px-6 md:px-16 lg:pl-56 "
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
